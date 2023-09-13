08b67a6b9ca53a806caac5a5f67bcd259ded8c5d
ceokartik <kartik@pvaindia.com>
2023-09-13 05:49:59 +0530

    delete

--- lib/model/audio_file_model.dart
+++
@@ -1,6 +0,0 @@
-class AudioFiles {
-  final String name;
-  final String path;
-  final int size;
-  AudioFiles({required this.name, required this.path, required this.size});
-}
--- lib/res/app_path.dart
+++
@@ -1,4 +0,0 @@
-class AppPaths{
-  static const albumPath='/storage/emulated/0/Music/Album';
-  static const musicPath='/storage/emulated/0/Music';
-}
